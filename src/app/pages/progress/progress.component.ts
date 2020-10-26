0210dbbbad68c0c5838fcc0e254eee74da08bea5
David Coello S <coellosuarezd@gmail.com>
2020-10-25 22:13:11 -0500

    Progress Bar

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,19 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
-  progrese1: number = 25;
-  progrese2: number = 55;
+  progreso1: number = 25;
+  progreso2: number = 55;
 
   get getProgreso1() {
-    return `${this.progrese1}%`
+    return `${this.progreso1}%`
   }
 
   get getProgreso2() {
-    return `${this.progrese2}%`
+    return `${this.progreso2}%`
   }
 
   cambioValorHijo(valor: number) {
-    this.progrese1 = valor;
+    this.progreso1 = valor;
   }
 
 }
